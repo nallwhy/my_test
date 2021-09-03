bdb7463844347528553df1fdcf1cdc2f6cf28fa5
json <json@momenti.tv>
2021-09-04 06:15:04 +0900

    Init project

---
+++ lib/my_test/application.ex
@@ -0,0 +1,32 @@
+defmodule MyTest.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Start the Telemetry supervisor
+      MyTestWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: MyTest.PubSub},
+      # Start the Endpoint (http/https)
+      MyTestWeb.Endpoint
+      # Start a worker by calling: MyTest.Worker.start_link(arg)
+      # {MyTest.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: MyTest.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    MyTestWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
---
+++ lib/my_test_web/views/layout_view.ex
@@ -0,0 +1,3 @@
+defmodule MyTestWeb.LayoutView do
+  use MyTestWeb, :view
+end
